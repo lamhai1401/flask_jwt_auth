d040eaa090eb81182d5583b41329cfaa1394075b
lamhai <lamthanhhai141@gmail.com>
2021-06-03 16:12:14 +0700

    fix jwt versiopn

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -q)
 
+export SECRET_KEY="project.server.config.DevelopmentConfig"
 export APP_SETTINGS="project.server.config.DevelopmentConfig"
 
 docker run --name jwt_auth -e POSTGRES_PASSWORD=mypassword -p 5432:5432 -d postgres:12.3-alpine
@@ -15,6 +16,6 @@ psql -h localhost -p 5432 --username=postgres --password
 # CREATE DATABASE
 # \q
 
-python3 manage.py create_db
-python3 manage.py db init
-python3 manage.py db migrate
+# python3 manage.py create_db
+# python3 manage.py db init
+# python3 manage.py db migrate
